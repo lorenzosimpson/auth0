10dce10d2702c2755deb27fc210a0285332cce21
Lorenzo Simpson <lorenzosimp@gmail.com>
2020-05-26 14:53:49 -0400

    slightly customizes auth0 starter for demo

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,11 +3,12 @@ import React, { Fragment } from "react";
 import Hero from "../components/Hero";
 import Content from "../components/Content";
 
+
 const Home = () => (
   <Fragment>
     <Hero />
-    <hr />
-    <Content />
+    {/* <hr /> */}
+    {/* <Content /> */}
   </Fragment>
 );
 
